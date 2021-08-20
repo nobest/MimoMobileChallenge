3707d34eb149568ed6fee62fd30aa206ba4dc599
Semiu Hassan <semiuhassan86@gmail.com>
2021-08-20 11:19:23 +0100

    Update the main activity to handle second fragment

--- app/src/main/java/com/meticulous/mimomobilechallenge/MainActivity.kt
+++ app/src/main/java/com/meticulous/mimomobilechallenge/MainActivity.kt
@@ -2,17 +2,42 @@ package com.meticulous.mimomobilechallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.meticulous.mimomobilechallenge.ui.main.CongratsFragment
+import com.meticulous.mimomobilechallenge.ui.main.MainActivityViewModel
 import com.meticulous.mimomobilechallenge.ui.main.MainFragment
+import com.meticulous.mimomobilechallenge.ui.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+            loadMainFragment()
         }
+
+        viewModel.uiNavState.observe(this) {
+            when (it) {
+                MainActivityViewModel.NavState.SHOW_CONGRATS_FRAGMENT -> {
+                    loadCongratsFragment()
+                }
+                else -> Unit
+            }
+        }
+    }
+
+    private fun loadMainFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment.newInstance(), MainFragment.TAG)
+            .commitNow()
+    }
+
+    private fun loadCongratsFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, CongratsFragment.newInstance(), CongratsFragment.TAG)
+            .commit()
     }
 }
