da45f9acda33e81107c6a584fe0b8a6a1cccff44
Semiu Hassan <semiuhassan86@gmail.com>
2021-08-20 11:13:10 +0100

    Create the repository class to handle data gathering

---
+++ app/src/main/java/com/meticulous/mimomobilechallenge/tools/LessonRepository.kt
@@ -0,0 +1,66 @@
+package com.meticulous.mimomobilechallenge.tools
+
+import android.util.Log
+import com.meticulous.mimomobilechallenge.models.Lesson
+import com.meticulous.mimomobilechallenge.models.LessonsData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LessonRepository {
+    private val TAG = "MIMO_Repository"
+    val lessons: MutableList<Lesson> = mutableListOf()
+    val lessonFetchedListeners = mutableSetOf<LessonFetchedListener>()
+    var lastIndex = 0
+
+    init {
+        fetchLessons()
+    }
+
+    private fun fetchLessons() {
+        Log.d(TAG, "fetchLessons called")
+        RestService.getRetrofit().lessons.enqueue(
+            object : Callback<LessonsData> {
+                override fun onResponse(call: Call<LessonsData>, response: Response<LessonsData>) {
+                    Log.d(
+                        TAG,
+                        "getLessons.onResponse got response: ${response.body()}"
+                    )
+                    response.body()?.let { lessonData ->
+                        lessons.clear()
+                        lessons.addAll(lessonData.lessons)
+                        lessonFetchedListeners.forEach { it.onLessonFetched() }
+                    }
+                }
+
+                override fun onFailure(call: Call<LessonsData>, t: Throwable) {
+                    Log.d(TAG, "getLessons.onFailure got Throwable: $t")
+                }
+            })
+    }
+
+    fun getNextLesson(): Lesson? {
+        Log.d(TAG, "getNextLesson called lastIndex: $lastIndex and size: ${lessons.size}")
+        if (lastIndex >= lessons.size) {
+            Log.w(TAG, "getNextLesson  returning null")
+            return null
+        }
+        return lessons[lastIndex++]
+    }
+
+    fun resetIndex() {
+        Log.d(TAG, "resetIndex called")
+        lastIndex = 0
+    }
+
+    fun registerForLessonFetched(listener: LessonFetchedListener) {
+        Log.d(TAG, "registerForLessonFetched called")
+        lessonFetchedListeners.add(listener)
+        if (lessons.isNotEmpty()) listener.onLessonFetched()
+    }
+
+    fun deregisterFromLessonFetched(listener: LessonFetchedListener) {
+        Log.d(TAG, "deregisterFromLessonFetched called")
+        lessonFetchedListeners.remove(listener)
+    }
+}
